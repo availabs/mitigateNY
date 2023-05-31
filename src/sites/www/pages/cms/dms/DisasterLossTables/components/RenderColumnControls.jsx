978efa75dd6a0e1eee2799955600fe203b804e7a
shaunaksangdod <shaunaksangdod@gmail.com>
2023-05-31 18:57:34 -0400

    disasters page comp

--- src/sites/www/pages/cms/dms/DisasterLossTables/components/RenderColumnControls.jsx
+++ src/sites/www/pages/cms/dms/DisasterLossTables/components/RenderColumnControls.jsx
@@ -1,19 +1,22 @@
 export const RenderColumnControls = ({cols, filters, setFilters}) => {
     return (
-        <div className={'flex space-between'}>
+        <div className={'flex flex-col sm:flex-row space-between'}>
             {
                 cols.map((c, i) => (
-                    <div className={'flex flex-col p-1'}>
-                        <label>{c} Filter: </label>
-                        <select
-                            className={'p-1 bg-blue-100'}
-                            value={i===0 ? 'text' : filters[c]}
-                            onChange={e => setFilters({...filters, ...{[c]: e.target.value}})}
-                            disabled={i === 0}
-                        >
-                            <option value={null}>None</option>
-                            <option value={'text'}>Text</option>
-                        </select>
+                    <div className={'flex flex-col justify-between p-1 border border-dashed border-blue-300 rounded-md w-[500px]'}>
+                        <div className={'font-normal'}>{c}</div>
+                        <div className={'w-full'}>
+                            <label className={'align-bottom'}> Filter: </label>
+                            <select
+                                className={'p-1 bg-white rounded-md align-bottom w-full'}
+                                value={i===0 ? 'text' : filters[c]}
+                                onChange={e => setFilters({...filters, ...{[c]: e.target.value}})}
+                                disabled={i === 0}
+                            >
+                                <option value={null}>None</option>
+                                <option value={'text'}>Text</option>
+                            </select>
+                        </div>
                     </div>
                 ))
             }
