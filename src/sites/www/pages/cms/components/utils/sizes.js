bfdfb545949cb4b6894c620a595470ed85b39793
alex <am3081@gmail.com>
2023-06-07 09:27:31 -0400

    changes

--- src/sites/www/pages/cms/components/utils/sizes.js
+++ src/sites/www/pages/cms/components/utils/sizes.js
@@ -9,26 +9,26 @@ export const sizeOptions = [
 ]
 
 const sizes = {
-    "2": 'col-span-8',
+    "2": 'col-span-6 lg:col-span-8',
     "1": 'col-span-6',
-    "2/3": 'col-span-4',
-    "1/2": 'col-span-3',
-    "1/3": 'col-span-2'
+    "2/3": 'col-span-6 md:col-span-4',
+    "1/2": 'col-span-6 md:col-span-3',
+    "1/3": 'col-span-6 md:col-span-2'
 }
 
 export const getSizeClass = (size, prevSize) => {
     if(["2"].includes(size)) { return sizes[size] }
 
     if(size === "2/3" && prevSize === '1/3' ) {
-        return `col-start-4 ${sizes[size]}`
+        return `md:col-start-3 lg:col-start-4 ${sizes[size]}`
     }
     if(size === "1/2" && prevSize === '1/2' ) {
-        return `col-start-5 ${sizes[size]}`
+        return `md:col-start-2 lg:col-start-5 ${sizes[size]}`
     }
     if(size === "1/3" && prevSize === '2/3' ) {
-        return `col-start-6 ${sizes[size]}`
+        return `md:col-start- 4 lg:col-start-6 ${sizes[size]}`
     } 
-    return `col-start-2 ${sizes[size]}`
+    return `lg:col-start-2 ${sizes[size]}`
 }
 
 export const sizeGridTemplate = {gridTemplateColumns: '1fr 170px 170px 170px 170px 170px 170px 1fr'}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
@@ -7,23 +7,22 @@ module.exports = {
   theme: {
     extend: {
       gridTemplateColumns: {
-        // 24 column grid
-        '13': 'repeat(13, minmax(0, 1fr))',
-        '24': 'repeat(24, minmax(0, 1fr))'
+        "footer": "1fr 170px 170px 170px 170px 170px 170px 1fr",
       },
        gridColumn: {
-         'span-13': 'span 13 / span 13',
-         'span-14': 'span 14 / span 14',
-         'span-15': 'span 15 / span 15',
-         'span-16': 'span 16 / span 16',
-         'span-17': 'span 17 / span 17',
-         'span-18': 'span 18 / span 18',
-         'span-19': 'span 19 / span 19',
-         'span-20': 'span 20 / span 20',
-         'span-21': 'span 21 / span 21',
-         'span-22': 'span 22 / span 22',
-         'span-23': 'span 23 / span 23',
-         'span-24': 'span 24 / span 24'
+
+         "span-13": "span 13 / span 13",
+         "span-14": "span 14 / span 14",
+         "span-15": "span 15 / span 15",
+         "span-16": "span 16 / span 16",
+         "span-17": "span 17 / span 17",
+         "span-18": "span 18 / span 18",
+         "span-19": "span 19 / span 19",
+         "span-20": "span 20 / span 20",
+         "span-21": "span 21 / span 21",
+         "span-22": "span 22 / span 22",
+         "span-23": "span 23 / span 23",
+         "span-24": "span 24 / span 24"
       }
     },
   },
