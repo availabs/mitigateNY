b8be368a577b91c9ceddf50aae4cb8c8251dafbc
shaunaksangdod <shaunaksangdod@gmail.com>
2023-07-17 21:49:29 -0400

    hmgp

--- src/sites/www/pages/cms/dms/DisasterLossStats/components/RenderDisasterLossStats.jsx
+++ src/sites/www/pages/cms/dms/DisasterLossStats/components/RenderDisasterLossStats.jsx
@@ -3,14 +3,18 @@ import {fnumIndex} from "~/utils/macros.jsx";
 import {Link} from "react-router-dom";
 import {Attribution} from "../../../components/attribution.jsx";
 
-const blockClass = `w-full h-[90px] bg-white p-5 text-center flex flex-col`,
+const blockClass = `w-full h-[90px] bg-white p-1 text-center flex flex-col`,
     blockLabelClass = `border-b-2`,
     blockValueClass = `font-medium text-xl pt-2`
 
-export const RenderDisasterLossStats = ({ totalLoss, ihpLoss, paLoss, sbaLoss, nfipLoss, usdaLoss, attributionData, baseUrl }) => {
+export const RenderDisasterLossStats = ({
+                                            totalLoss, ihpLoss, paLoss, sbaLoss,
+                                            nfipLoss, usdaLoss, hmgpFunding,
+                                            attributionData, baseUrl
+}) => {
    return (
        <React.Fragment>
-           <div className={"w-full grid grid-cols-6 gap-4 place-content-stretch mt-5"}>
+           <div className={"w-full grid grid-cols-7 gap-4 place-content-stretch mt-5"}>
                <div className={blockClass}>
                    <label className={blockLabelClass}>Total Loss</label>
                    <span className={blockValueClass}>{
@@ -45,6 +49,12 @@ export const RenderDisasterLossStats = ({ totalLoss, ihpLoss, paLoss, sbaLoss, n
                    <label className={blockLabelClass}>USDA Loss</label>
                    <span className={blockValueClass}>{
                        fnumIndex(usdaLoss, 2, true)
+                   }</span>
+               </div>
+               <div className={blockClass}>
+                   <label className={`${blockLabelClass}`}>HMGP Funding</label>
+                   <span className={blockValueClass}>{
+                       fnumIndex(hmgpFunding, 2, true)
                    }</span>
                </div>
            </div>
