ccc0839e07adb6becd0e1c9b45f3d41cbb8c8f0b
shaunaksangdod <shaunaksangdod@gmail.com>
2023-11-07 16:03:26 -0500

    legend

--- src/sites/www/pages/cms/dms/BuildingsMap/components/RenderLegend.jsx
+++ src/sites/www/pages/cms/dms/BuildingsMap/components/RenderLegend.jsx
@@ -15,12 +15,14 @@ const floodPlainColors = {
 
 export const RenderLegend = ({floodPlain=[], hazard=[]}) =>
     (
-        <div className={"flex flex-rows flex-wrap gap-2 text-xs align-middle"}>
-            {
-                Array.isArray(floodPlain) && floodPlain?.length ?
-                    floodPlain.map(fp => {
-                        return (
-                            <div className={"h-full flex"} key={'nonDeclaredDisasters'}>
+        <div className={'flex flex-col'}>
+            <label>State-owned assets in high hazard risk areas</label>
+            <div className={"flex flex-rows flex-wrap gap-2 text-xs align-middle"}>
+                {
+                    Array.isArray(floodPlain) && floodPlain?.length ?
+                        floodPlain.map(fp => {
+                            return (
+                                <div className={"h-full flex"} key={'nonDeclaredDisasters'}>
                             <span className={"rounded-full m-1 shrink-0"}
                                   style={{
                                       height: "10px",
@@ -28,25 +30,26 @@ export const RenderLegend = ({floodPlain=[], hazard=[]}) =>
                                       backgroundColor: floodPlainColors[fp]?.color
                                   }}
                             />
-                                <label className={"pl-1"}>{floodPlainColors[fp]?.name}</label>
-                            </div>
-                        )
-                    }) : null
-            }
-            {
-                Object.keys(hazardsMeta)
-                    .filter(key => !hazard || hazard.includes(key))
-                    .sort((a,b) => hazardsMeta[a].name.localeCompare(hazardsMeta[b].name))
-                    .map(key =>
-                        <div className={"h-full flex flex-wrap"} key={key}>
+                                    <label className={"pl-1"}>{floodPlainColors[fp]?.name}</label>
+                                </div>
+                            )
+                        }) : null
+                }
+                {
+                    Object.keys(hazardsMeta)
+                        .filter(key => !hazard || hazard.includes(key))
+                        .sort((a,b) => hazardsMeta[a].name.localeCompare(hazardsMeta[b].name))
+                        .map(key =>
+                            <div className={"h-full flex flex-wrap"} key={key}>
                             <span className={"rounded-full m-1 shrink-0"}
                                   style={{
                                       height: "10px",
                                       width: "10px",
                                       backgroundColor: get(hazardsMeta, [key, "color"], "#d0d0ce")
                                   }}/>
-                            <label className={"pl-1"}>{hazardsMeta[key].name}</label>
-                        </div>)
-            }
+                                <label className={"pl-1"}>{hazardsMeta[key].name}</label>
+                            </div>)
+                }
+            </div>
         </div>
     );
