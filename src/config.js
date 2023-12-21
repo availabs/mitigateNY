89925ab95656ed2a1cb051fafba1c7cb99554aa6
shaunaksangdod <shaunaksangdod@gmail.com>
2023-12-21 04:25:14 -0500

    hideSection flag

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ let CLIENT_HOST = "transportny.org";
 let DAMA_HOST = "https://graph.availabs.org";
 
 if (process.env.NODE_ENV === "development") {
-  // API_HOST = "http://localhost:4444";
+  API_HOST = "http://localhost:4444";
   // AUTH_HOST = "http://localhost:4444/auth"
   // CLIENT_HOST = "localhost:3000";
   DAMA_HOST = "https://graph.availabs.org";
