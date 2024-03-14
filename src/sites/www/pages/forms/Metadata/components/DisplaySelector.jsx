8180ce4db05950fd2a38dd067ab646f3701be584
shaunaksangdod <shaunaksangdod@gmail.com>
2024-03-14 14:09:40 -0400

    minor

--- src/sites/www/pages/forms/Metadata/components/DisplaySelector.jsx
+++ src/sites/www/pages/forms/Metadata/components/DisplaySelector.jsx
@@ -7,9 +7,9 @@ export const DisplaySelector = ({update, metadata, setMetadata, col, value}) => 
         await editMetadata({update, metadata, setMetadata, col, value: {display: e.target.value}});
     }, [col, metadata]);
 
-    return (<div className={'border'}>
+    return (<div className={'border border-blue-100'}>
         <select
-            className="pl-3 pr-4 py-2.5 border border-blue-100 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
+            className="appearance-auto pl-3 pr-4 py-2.5 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
             value={value}
             onChange={onChange}
             // disabled={value === 'calculated-column'}
