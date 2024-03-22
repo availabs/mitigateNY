e59c3c81972aed088ea13f2ef3263673ae2cf605
Paul Tomchik <pauljtomchik@gmail.com>
2024-03-22 08:52:04 -0400

    HazmitDataTypes/gis_least_common_geographic_units

---
+++ src/pages/HazmitDataTypes/gis_least_common_geographic_units/index.jsx
@@ -0,0 +1,14 @@
+//  Where the datatype name comes from:
+//    https://cartogis.org/docs/proceedings/archive/auto-carto-2/pdf/topological-information-systems-for-geographic-representation.pdf
+import React from 'react';
+import Create from './create'
+
+const LCGUConfig = {
+  
+  sourceCreate: {
+    name: 'Create',
+    component: Create
+  }
+}
+
+export default LCGUConfig
