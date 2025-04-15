95427b7a3cacf07ef93d2867edcaed6f4ba26db2
shaunaksangdod <shaunaksangdod@gmail.com>
2025-04-15 14:15:22 -0400

    graph theme

--- src/dms_themes/mnyv1/index.jsx
+++ src/dms_themes/mnyv1/index.jsx
@@ -366,7 +366,13 @@ const theme = {
     settingLabel: 'text-gray-900 font-regular min-w-fit'
   },
   graph: {
-    text: 'text-[#2D3E4C] font-[Oswald] font-semibold text-[12px] leading-[100%] tracking-[0px] uppercase'
+    text: 'text-[#2D3E4C] font-[Oswald] font-semibold text-[12px] leading-[100%] tracking-[0px] uppercase',
+    headerWrapper: 'grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-x-1 gap-y-0.5',
+    columnControlWrapper: `px-1 font-semibold border bg-gray-50 text-gray-500`,
+    scaleWrapper: 'flex rounded-[8px] divide-x border w-fit border-[#E0EBF0] overflow-hidden',
+    scaleItem: 'px-[12px] py-[7px] font-[Oswald] font-medium text-[12px] text-[#2D3E4C] text-center leading-[100%] tracking-[0px] uppercase cursor-pointer',
+    scaleItemActive: 'bg-white',
+    scaleItemInActive: 'bg-[#F3F8F9]',
   },
   icon: {
     icon: 'text-slate-400 hover:text-blue-500 size-4'
