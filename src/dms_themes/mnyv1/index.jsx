24edf9cc127e85c0c22c586e918982d6d1995f89
shaunaksangdod <shaunaksangdod@gmail.com>
2025-04-21 12:09:53 -0400

    graph dark mode

--- src/dms_themes/mnyv1/index.jsx
+++ src/dms_themes/mnyv1/index.jsx
@@ -392,6 +392,7 @@ const theme = {
   },
   graph: {
     text: 'text-[#2D3E4C] font-[Oswald] font-semibold text-[12px] leading-[100%] tracking-[0px] uppercase',
+    darkModeText: 'bg-transparent text-white font-[Oswald] font-semibold text-[12px] leading-[100%] tracking-[0px] uppercase',
     headerWrapper: 'grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-x-1 gap-y-0.5',
     columnControlWrapper: `px-1 font-semibold border bg-gray-50 text-gray-500`,
     scaleWrapper: 'flex rounded-[8px] divide-x border w-fit border-[#E0EBF0] overflow-hidden',
