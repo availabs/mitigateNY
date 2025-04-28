aa8a0d078ee006812343f06bc4ca03bd57bf66b1
Alex Muro <am3081@gmail.com>
2025-04-28 16:29:27 -0400

    test 123

--- src/dms_themes/mnyv1/index.jsx
+++ src/dms_themes/mnyv1/index.jsx
@@ -30,7 +30,7 @@ const Logo = ({sideNav}) => {
 const theme = {
   Icons,
   navOptions: {
-    logo: <Logo />,//'',//<Link to='/' className='h-12 flex px-4 items-center'><div className='rounded-full h-8 w-8 bg-blue-500 border-2 border-blue-300 hover:bg-blue-600' /></Link>, //<Link to='/' className='h-12 flex px-4 items-center'>LOGO</Link>,
+    logo: <Logo />,//'',//<Link to='/' className='h-12 flex px-4 items-center'><div className='rounded-full h-8 w-8 bg-blue-500  border-blue-300 hover:bg-blue-600' /></Link>, //<Link to='/' className='h-12 flex px-4 items-center'>LOGO</Link>,
     sideNav: {
       size: 'none',
       search: 'none',
@@ -103,7 +103,7 @@ const theme = {
     container: `bg-[linear-gradient(0deg,rgba(244,244,244,0.96),rgba(244,244,244,0.96)),url('/themes/mny/topolines.png')]  bg-[size:500px] pb-[4px]`,//`bg-gradient-to-b from-[#F4F4F4] to-[#F4F4F4] bg-[url('/themes/mny/topolines.png')] `,
     wrapper1: 'w-full h-full flex-1 flex flex-col ', // first div inside Layout
     wrapper2: 'w-full h-full flex-1 flex flex-row lg:pt-[118px] ', // inside page header, wraps sidebar
-    wrapper3: 'flex flex-1 w-full border-2 flex-col border shadow-md bg-white rounded-lg relative text-md font-light leading-7 p-4 h-full min-h-[calc(100vh_-_102px)]' , // content wrapepr
+    wrapper3: 'flex flex-1 w-full  flex-col border shadow-md bg-white rounded-lg relative text-md font-light leading-7 p-4 h-full min-h-[calc(100vh_-_102px)]' , // content wrapepr
     iconWrapper : 'z-10 absolute right-[10px] top-[5px]',
     icon: 'text-slate-400 hover:text-blue-500'
   },
@@ -127,54 +127,44 @@ const theme = {
   sectionGroup: {
     default: {
       wrapper1: 'w-full h-full flex-1 flex flex-row pt-2', // inside page header, wraps sidebar
-      wrapper2: 'flex flex-1 w-full border-2 flex-col border shadow-md bg-white rounded-lg relative text-md font-light leading-7 p-4 h-full min-h-[200px]' , // content wrapepr
+      wrapper2: 'flex flex-1 w-full  flex-col  shadow-md bg-white rounded-lg relative text-md font-light leading-7 p-4 h-full min-h-[200px]' , // content wrapepr
       iconWrapper : 'z-5 absolute right-[10px] top-[5px]',
       icon: 'text-slate-400 hover:text-blue-500',
-      sideNavContainer1: 'w-64 hidden xl:block',
-      sideNavContainer2: 'w-64 sticky top-20 hidden xl:block'
+      sideNavContainer1: 'hidden xl:block',
+      sideNavContainer2: 'min-w-[302px] max-w-[302px] sticky top-20 hidden xl:block h-[100vh_-_102px] pr-2'
     },
     content: {
       wrapper1: 'w-full h-full flex-1 flex flex-row lg:pt-[118px] ', // inside page header, wraps sidebar
-      wrapper2: 'flex flex-1 w-full border-2 flex-col border shadow-md bg-white rounded-lg relative text-md font-light leading-7 p-4 h-full min-h-[calc(100vh_-_102px)]' , // content wrapepr
+      wrapper2: 'flex flex-1 w-full  flex-col  shadow-md bg-white rounded-lg relative text-md font-light leading-7 p-4 h-full min-h-[calc(100vh_-_102px)]' , // content wrapepr
       iconWrapper : 'z-5 absolute right-[10px] top-[5px]',
-      icon: 'text-slate-400 hover:text-blue-500',
-      sideNavContainer1: 'w-64 hidden xl:block',
-      sideNavContainer2: 'w-64 sticky top-20 hidden xl:block'
+      icon: 'text-slate-400 hover:text-blue-500'
     },
     darkSection: {
       wrapper1: `w-full h-full flex-1 flex flex-row -my-8 py-10 bg-[linear-gradient(0deg,rgba(33,52,64,.96),rgba(55,87,107,.96)),url('/themes/mny/topolines.png')]  bg-[size:500px] pb-[4px]`, // inside page header, wraps sidebar
       wrapper2: 'max-w-[1440px]  xl:px-[64px] md:px-4 mx-auto',
       wrapper3: 'flex flex-1 w-full  flex-col  relative text-md font-light leading-7 p-4 h-full min-h-[200px]' , // content wrapepr
       iconWrapper : 'z-5 absolute right-[10px] top-[5px]',
-      icon: 'text-slate-400 hover:text-blue-500',
-      sideNavContainer1: 'w-64 hidden xl:block',
-      sideNavContainer2: 'w-64 sticky top-20 hidden xl:block'
+      icon: 'text-slate-400 hover:text-blue-500'
     },
     lightCentered: {
       wrapper1: `w-full h-full flex-1 flex flex-row pb-[4px] `, // inside page header, wraps sidebar
       wrapper2: 'max-w-[1440px]  xl:px-[64px] md:px-4 mx-auto',
-      wrapper3: 'flex flex-1 w-full border shadow-md bg-white rounded-lg  flex-col  relative text-md font-light leading-7 p-4 h-full min-h-[200px]' , // content wrapepr
+      wrapper3: 'flex flex-1 w-full  shadow-md bg-white rounded-lg  flex-col  relative text-md font-light leading-7 p-4 h-full min-h-[200px]' , // content wrapepr
       iconWrapper : 'z-5 absolute right-[10px] top-[5px]',
-      icon: 'text-slate-400 hover:text-blue-500',
-      sideNavContainer1: 'w-64 hidden xl:block',
-      sideNavContainer2: 'w-64 sticky top-20 hidden xl:block'
+      icon: 'text-slate-400 hover:text-blue-500'
     },
     clearCentered: {
       wrapper1: `w-full h-full flex-1 flex flex-row -mt-3`, // inside page header, wraps sidebar
       wrapper2: 'max-w-[1440px] w-full xl:px-[48px] mx-auto',
       wrapper3: 'flex flex-1 w-full flex-col relative h-full min-h-[200px]' , // content wrapepr
       iconWrapper : 'z-5 absolute right-[10px] top-[5px]',
-      icon: 'text-slate-400 hover:text-blue-500',
-      sideNavContainer1: 'w-64 hidden xl:block',
-      sideNavContainer2: 'w-64 sticky top-20 hidden xl:block'
+      icon: 'text-slate-400 hover:text-blue-500'
     },
     header: {
       wrapper1: 'w-full h-full flex-1 flex flex-row', // inside page header, wraps sidebar
       wrapper2: 'flex flex-1 w-full  flex-col  relative min-h-[200px]' , // content wrapepr
       iconWrapper : 'z-5 absolute right-[10px] top-[5px]',
-      icon: 'text-slate-400 hover:text-blue-500',
-      sideNavContainer1: 'hidden',
-      sideNavContainer2: 'hidden',
+      icon: 'text-slate-400 hover:text-blue-500'
     }
   },
   pageControls: {
