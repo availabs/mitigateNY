e21cfb0e0017712f79a1506d77a8dfc2b11dd63c
shaunaksangdod <shaunaksangdod@gmail.com>
2025-07-07 14:07:09 -0400

    admin config update

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
     return (
       <DmsSite
         dmsConfig = {
-          adminConfig({
+          adminConfig[0]({
               app, type, baseUrl: adminBaseUrl,
               // API_HOST
           })
