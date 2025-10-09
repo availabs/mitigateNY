6e0350fc0b2ac1ff685d18bec2c5e8bf88d103c9
shaunaksangdod <shaunaksangdod@gmail.com>
2025-10-09 14:19:31 -0400

    datasets theme

--- src/dms_themes/index.js
+++ src/dms_themes/index.js
@@ -1,12 +1,12 @@
 import mnyv1 from './mnyv1'
 import mny_admin from './mny_admin'
 import mny_auth from "./mny_auth";
-
+import mny_datasets from "./mny_datasets"
 
 export default {
 	mny: mnyv1,
   mnyv1,
   mny_admin,
-  mny_auth
-
+  mny_auth,
+  mny_datasets
 }
