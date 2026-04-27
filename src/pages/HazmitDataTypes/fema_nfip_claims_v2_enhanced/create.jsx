859899b4a60274811cb0c59b301b2d3a4981e364
shaunaksangdod <shaunaksangdod@gmail.com>
2026-04-27 13:32:49 -0400

    nfip enhanced fix

--- src/pages/HazmitDataTypes/fema_nfip_claims_v2_enhanced/create.jsx
+++ src/pages/HazmitDataTypes/fema_nfip_claims_v2_enhanced/create.jsx
@@ -7,7 +7,7 @@ import {useNavigate} from "react-router";
 import { DamaContext } from "~/pages/DataManager/store";
 import { RenderVersions, getType } from "../utils/macros";
 
-const CallServer = async ({rtPfx, baseUrl, source, newVersion, navigate, viewNFIP, viewDDS, viewCounty, viewJurisdiction, user}) => {
+const CallServer = async ({rtPfx, baseUrl, source, newVersion, navigate, viewNFIP, viewDDS, viewCounty, viewJurisdiction, viewBlockGroup, user}) => {
     const viewMetadata = [viewNFIP.view_id, viewDDS.view_id, viewCounty.view_id, viewJurisdiction.view_id];
 
     const url = `${rtPfx}/hazard_mitigation/enhance-nfip-claims-v2`;
@@ -16,7 +16,7 @@ const CallServer = async ({rtPfx, baseUrl, source, newVersion, navigate, viewNFI
         source_name: source.name,
         existing_source_id: source.source_id,
         view_dependencies: JSON.stringify(viewMetadata),
-        version: newVersion,
+        version: newVersion || (new Date()).toLocaleString(),
 
         user_id: user.id,
         email: user.email,
@@ -28,7 +28,9 @@ const CallServer = async ({rtPfx, baseUrl, source, newVersion, navigate, viewNFI
         county_schema: viewCounty.table_schema,
         county_table: viewCounty.table_name,
         jurisdiction_schema: viewJurisdiction.table_schema,
-        jurisdiction_table: viewJurisdiction.table_name
+        jurisdiction_table: viewJurisdiction.table_name,
+        blockgroup_schema: viewBlockGroup.table_schema,
+        blockgroup_table: viewBlockGroup.table_name
     });
     
     const stgLyrDataRes = await fetch(url, {
@@ -43,21 +45,24 @@ const CallServer = async ({rtPfx, baseUrl, source, newVersion, navigate, viewNFI
     const resJson = await stgLyrDataRes.json();
     console.log('res', resJson);
 
-    navigate(resJson.etl_context_id ? `${baseUrl}/task/${resJson.etl_context_id}` : resJson.source_id ? `${baseUrl}/source/${resJson.source_id}/versions` : baseUrl);
+    // navigate(resJson.etl_context_id ? `${baseUrl}/task/${resJson.etl_context_id}` : resJson.source_id ? `${baseUrl}/source/${resJson.source_id}/versions` : baseUrl);
 }
 
 const Create = ({ source, newVersion, baseUrl, context }) => {
     const navigate = useNavigate();
-    const { pgEnv, user, falcor } = React.useContext(context || DamaContext)
+    const { user, falcor, datasources } = React.useContext(context || DamaContext)
+    const pgEnv = datasources.find(d => d.type === 'external')?.env;
     const rtPfx = getDamaApiRoutePrefix(pgEnv);
 
     const [viewNFIP, setViewNFIP] = React.useState();
     const [viewDDS, setViewDDS] = React.useState();
     const [viewCounty, setViewCounty] = React.useState();
+    const [viewBlockGroup, setViewBlockGroup] = React.useState();
     const [viewJurisdictions, setViewJurisdictions] = React.useState();
     const [versionsNFIP, setVersionsNFIP] = React.useState({sources:[], views: []});
     const [versionsDDS, setVersionsDDS] = React.useState({sources:[], views: []});
     const [versionsCounty, setVersionsCounty] = React.useState({sources:[], views: []});
+    const [versionsBlockGroup, setVersionsBlockGroup] = React.useState({sources:[], views: []});
     const [versionsJurisdictions, setVersionsJurisdictions] = React.useState({sources:[], views: []});
 
     React.useEffect(() => {
@@ -65,6 +70,7 @@ const Create = ({ source, newVersion, baseUrl, context }) => {
             await getSrcViews({rtPfx, falcor, pgEnv, setVersions: setVersionsNFIP, type: 'fima_nfip_claims_v2'});
             await getSrcViews({rtPfx, falcor, pgEnv, setVersions: setVersionsDDS, type: 'disaster_declarations_summaries_v2'});
             await getSrcViews({rtPfx, falcor, pgEnv, setVersions: setVersionsCounty, type: 'tl_county'});
+            await getSrcViews({rtPfx, falcor, pgEnv, setVersions: setVersionsBlockGroup, type: 'tl_blockgroup'});
             await getSrcViews({rtPfx, falcor, pgEnv, setVersions: setVersionsJurisdictions, type: 'jurisdictions'});
         }
         fetchData()
@@ -75,6 +81,7 @@ const Create = ({ source, newVersion, baseUrl, context }) => {
             {RenderVersions({value: viewNFIP, setValue: setViewNFIP, versions: versionsNFIP, type: 'NFIP'})}
             {RenderVersions({value: viewDDS, setValue: setViewDDS, versions: versionsDDS, type: 'Disaster Declarations Summary'})}
             {RenderVersions({value: viewCounty, setValue: setViewCounty, versions: versionsCounty, type: 'County'})}
+            {RenderVersions({value: viewBlockGroup, setValue: setViewBlockGroup, versions: versionsBlockGroup, type: 'Block Group'})}
             {RenderVersions({value: viewJurisdictions, setValue: setViewJurisdictions, versions: versionsJurisdictions, type: 'Jurisdictions'})}
             <button
                 className={`mx-6 p-1 text-sm border-2 border-gray-200 rounded-md`}
@@ -83,6 +90,7 @@ const Create = ({ source, newVersion, baseUrl, context }) => {
                     viewNFIP: versionsNFIP.views.find(v => v.view_id === parseInt(viewNFIP)),
                     viewDDS: versionsDDS.views.find(v => v.view_id === parseInt(viewDDS)),
                     viewCounty: versionsCounty.views.find(v => v.view_id === parseInt(viewCounty)),
+                    viewBlockGroup: versionsBlockGroup.views.find(v => v.view_id === parseInt(viewBlockGroup)),
                     viewJurisdiction: versionsJurisdictions.views.find(v => v.view_id === parseInt(viewJurisdictions))
             })}> {source.source_id ? 'Add View' : 'Add Source'}</button>
         </div>
